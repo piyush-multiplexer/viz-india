d0e494d4d606eb4ff8f53c12682399913ab574df
Piyush <thesourcepedia@gmail.com>
2022-10-06 16:08:06 +0530

    feat(map): draw indian map with google geo chart and refactor code

---
+++ src/components/IndiaMap.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  google.charts.load("current", { packages: ["geochart"] });
+
+  google.charts.setOnLoadCallback(drawCountryChart);
+  function drawCountryChart() {
+    var data = new google.visualization.arrayToDataTable([
+      ["State", "Select"],
+      ["IN-AP", 0],
+      ["IN-AR", 0],
+      ["IN-AS", 0],
+      ["IN-BR", 0],
+      ["IN-CT", 0],
+      ["IN-GA", 0],
+      ["IN-GJ", 0],
+      ["IN-HR", 0],
+      ["IN-HP", 0],
+      ["IN-JH", 0],
+      ["IN-KA", 0],
+      ["IN-KL", 0],
+      ["IN-MP", 0],
+      ["IN-MH", 0],
+      ["IN-MN", 0],
+      ["IN-ML", 0],
+      ["IN-MZ", 0],
+      ["IN-NL", 0],
+      ["IN-OR", 0],
+      ["IN-PB", 0],
+      ["IN-RJ", 0],
+      ["IN-SK", 0],
+      ["IN-TN", 0],
+      ["IN-TG", 0],
+      ["IN-TR", 0],
+      ["IN-UT", 0],
+      ["IN-UP", 0],
+      ["IN-WB", 0],
+      ["IN-AN", 0],
+      ["IN-CH", 0],
+      ["IN-DH", 0],
+      ["IN-DL", 0],
+      ["IN-JK", 0],
+      ["IN-LA", 0],
+      ["IN-LD", 0],
+      ["IN-PY", 0],
+    ]);
+
+    var options = {
+      region: "IN",
+      magnifyingGlass: {
+        enable: true,
+      },
+      displayMode: "regions",
+      resolution: "provinces",
+      title: "How Much Pizza I Ate Last Night",
+      width: 920,
+      height: 780,
+    };
+
+    var chart = new google.visualization.GeoChart(
+      document.getElementById("chart_div")
+    );
+    chart.draw(data, options);
+  }
+});
+</script>
+
+<template>
+  <div id="chart_div"></div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,19 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <div class="greetings">
+      <h1 class="green">VIZ-I</h1>
+    </div>
+    <IndiaMap />
   </main>
 </template>
+<script setup>
+import IndiaMap from "../components/IndiaMap.vue";
+</script>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+</style>
